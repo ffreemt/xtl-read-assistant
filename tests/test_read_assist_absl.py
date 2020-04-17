92f423252a750c424f017c5bbce8403e0bb58a81
ffreemt <yucongo+fmt@gmail.com>
2020-04-17 14:13:09 +0800

    if: steps.codecove.exitcode

---
+++ tests/test_read_assist_absl.py
@@ -0,0 +1,12 @@
+from xtl_read_assistant import read_assist
+from absl import flags
+
+
+def test_absl():
+    """ test absl """
+    # app.run(read_assist)
+    flags.FLAGS(['app', ])
+    m_lang, s_lang, t_lang = read_assist([], debug=True)
+    assert m_lang == "zh"
+    assert s_lang == "en"
+    assert t_lang == "de"
